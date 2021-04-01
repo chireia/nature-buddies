21a2893ee6cecd4f38012cf7e05ba581dbac5567
Rodrigo Chireia <rodrigochireia@hotmail.com>
2021-04-01 03:21:29 -0400

    Add some useful styled components to create simple panels

---
+++ src/components/styled/Panel.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Panel = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+`
+
+export const PanelTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.3em;
+  font-weight: 600;
+  padding: 14px;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.05);
+`
